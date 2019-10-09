da541dba727d496075af645a807f44c1ca6e6e07
Jorge Armando Pérez Garay <joarpega@gmail.com>
2019-10-09 11:00:41 -0500

    [BN-5] feat Conexion a la base de datos y log

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 declare var firebase: any;
+declare var database: any;
 
 @Component({
   selector: 'app-home',
@@ -10,22 +11,33 @@ declare var firebase: any;
 export class HomePage implements OnInit {
 
   intervalRef: any;
+  user = firebase.auth().currentUser;
 
   constructor() { }
 
   ngOnInit() {
     firebase.auth().onAuthStateChanged(user => {
+      database = firebase.database();
+      this.user = user;
       if (user) {
         this.intervalRef = setInterval(() => {
+
           console.log('Guardando registro');
+          const timestamp = Date.now();
+          database.ref(timestamp).set({
+            user: this.user.email,
+            timestamp
+          });
+
         }, 10000);
         console.log('User Login', user);
       } else {
         console.log('User is not login');
       }
 
-      console.log(user);
     });
+
+
   }
 
   singOut() {
